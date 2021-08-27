0ae6d48928b01c722206f1db16c04784a7cb9621
SErAphLi <Seraphlivery@gmail.com>
2021-08-27 15:29:15 +0800

    Update

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,16 +8,18 @@ public class Ball : MonoBehaviour
 
     public Color color;
     public GM gm;
+    private bool _enabled = false;
 
     // Start is called before the first frame update
     void Start()
     {
         color = gm.colorMap[type];
         var sr = GetComponent<SpriteRenderer>();
-        sr.color = color;
+        sr.color = new Color(color.r * 0.6f, color.g * 0.6f, color.b * 0.6f);
         var rb = GetComponent<Rigidbody2D>();
         var m_NewForce = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
         rb.AddForce(m_NewForce.normalized, ForceMode2D.Impulse);
+        _enabled = true;
     }
 
     // Update is called once per frame
@@ -27,6 +29,11 @@ public class Ball : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        if (!_enabled)
+        {
+            return;
+        }
+
         var go = col.gameObject;
         if (go.name.Contains("Square") && go.layer >= 6)
         {
