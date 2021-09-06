c8800e54e8dc9a6d4aae125977061f1e69a56272
SErAphLi <seraphlivery@gmail.com>
2021-09-07 01:33:11 +0800

    Update

--- Assets/Scripts/TestData.cs
+++ Assets/Scripts/TestData.cs
@@ -19,34 +19,34 @@ public struct CTab
 
     public static Color BallOrange => HexToColor("#FFA726");
     public static Color TileOrange => HexToColor("#FB8C00");
-    
+
     public static Color BallYellow => HexToColor("#FFEE58");
     public static Color TileYellow => HexToColor("#FDD835");
 
     public static Color BallGreen => HexToColor("#66BB6A");
     public static Color TileGreen => HexToColor("#43A047");
-    
+
     public static Color BallCyan => HexToColor("#26C6DA");
     public static Color TileCyan => HexToColor("#00ACC1");
-    
+
     public static Color BallBlue => HexToColor("#29B6F6");
     public static Color TileBlue => HexToColor("#039BE5");
-    
+
     public static Color BallPurple => HexToColor("#7E57C2");
     public static Color TilePurple => HexToColor("#5E35B1");
-    
+
     public static Color BallMagenta => HexToColor("#AB47BC");
     public static Color TileMagenta => HexToColor("#8E24AA");
-    
+
     public static Color BallPink => HexToColor("#EC407A");
     public static Color TilePink => HexToColor("#D81B60");
-    
+
     public static Color BallIndigo => HexToColor("#5C6BC0");
     public static Color TileIndigo => HexToColor("#3949AB");
-    
+
     public static Color BallBrown => HexToColor("#8D6E63");
     public static Color TileBrown => HexToColor("#6D4C41");
-    
+
     public static Color BallGrey => HexToColor("#BDBDBD");
     public static Color TileGrey => HexToColor("#757575");
 
@@ -88,9 +88,19 @@ public class TestData : MonoBehaviour
         new Team() {index = 14, ballColor = CTab.BallGrey, tileColor = CTab.TileGrey},
     };
 
+    public Dictionary<int, Team> _teamMap;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        _teamMap = new Dictionary<int, Team>();
+        foreach (var team in teams)
+        {
+            // Add index to team map
+            _teamMap[team.index] = team;
+            // Ignore Layer Collision
+            Physics2D.IgnoreLayerCollision(team.index, team.index);
+        }
     }
 
     // Update is called once per frame
